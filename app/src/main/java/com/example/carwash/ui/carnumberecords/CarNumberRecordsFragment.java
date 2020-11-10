4102f642003f97200580470cc259a7e83283d106
cocoloco93 <nicksmile93@gmail.com>
2020-11-10 19:37:50 +0630

    11/10

--- app/src/main/java/com/example/carwash/ui/carnumberecords/CarNumberRecordsFragment.java
+++ app/src/main/java/com/example/carwash/ui/carnumberecords/CarNumberRecordsFragment.java
@@ -1,7 +1,9 @@
 package com.example.carwash.ui.carnumberecords;
 
 import android.graphics.Color;
+import android.graphics.ColorFilter;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +15,8 @@ import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
 import com.github.mikephil.charting.utils.ColorTemplate;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import java.util.ArrayList;
@@ -24,9 +28,13 @@ import java.util.List;
  * create an instance of this fragment.
  */
 public class CarNumberRecordsFragment extends Fragment {
+     PieChart pie;
+    float rainfall[] ={98.8f,123.8f,161.6f,24.2f,52f,35.4f,13.8f,73.4f,203.4f,240.4f,240.2f,159.7f};
+    String monthName[] = {"Jan","Feb","Mar","April","May","June","July",
+            "Aug","Sep","Oct","Nov","Dec"};
 
 
-    // TODO: Rename parameter arguments, choose names that match
+        // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
@@ -77,9 +85,33 @@ public class CarNumberRecordsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_car_number_records, container, false);
-    }
+        View view= inflater.inflate(R.layout.fragment_car_number_records, container, false);
+
+        pie = (PieChart)view.findViewById(R.id.pie);
+        List<PieEntry> pieEntries = new ArrayList<>();
+        for (int i = 0; i < monthName.length; i++) {
+            pieEntries.add(new PieEntry(rainfall[i], monthName[i]));
+
+            PieDataSet PiedataSet = new PieDataSet(pieEntries,"");
+            PiedataSet.setColors(ColorTemplate.JOYFUL_COLORS);
+            PiedataSet.setValueTextColor(Color.BLACK);
+            PiedataSet.setValueTextSize(10f);
+            PieData data = new PieData(PiedataSet);
+
+            // Design Pie
+            pie.setCenterText("Total Numbers of Cars ");
+            pie.setCenterTextRadiusPercent(80);
+            pie.setData(data);
+            pie.setHoleRadius(70);
+            pie.highlightValues(null);
+            pie.setTransparentCircleRadius(1);
+            pie.setRotationEnabled(false);
+            pie.animateY(800);
 
 
+
+
+        }return view;
+}
 }
     
